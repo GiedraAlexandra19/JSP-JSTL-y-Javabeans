b9503228cfa1a1bc124e277d60e789db3ead6dd7
Alexandra <apinoh@unsa.edu.pe>
2021-05-25 16:04:50 -0500

    Entregado

---
+++ src/main/java/pe/edu/unsa/epcc/dbp/Servlet.java
@@ -0,0 +1,50 @@
+package pe.edu.unsa.epcc.dbp;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.getWriter().append("Served at: ").append(request.getContextPath());
+    	String yourName= request.getParameter("yourName");
+    	Person person= new Person();
+    	person.setName(yourName);
+    	person.setAge(17);
+    	request.setAttribute("person", person);
+    	RequestDispatcher dispatcher=
+    				request.getRequestDispatcher("welcome.jsp");
+    				dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
